cdc2e0666480a6d5617c1da010c5954c34d3eda3
Ollie570 <oliveiramiguel142@gmail.com>
2023-01-20 14:29:57 -0300

    Aula 4

--- projeto/script.js
+++ projeto/script.js
@@ -1,11 +1,30 @@
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-    run: ['01-01', '01-02', '01-06', '01-07', '01-08'],
-    takePills: ['01-03'],
-    journal: ['01-02']
+button.addEventListener('click', add)
+//Salvando os dados:
+form.addEventListener("change", save)
+
+function add() {
+    //Deixando a mudança de dias dinamica: 
+    const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+    const dayExists = nlwSetup.dayExists(today)
+
+    if(dayExists) {
+        alert("Dia já foi adicionado 🔴")
+        return
+    }
+
+    alert('Adicionado com sucesso ✔')
+    nlwSetup.addDay(today)
 }
 
+//Função de salvar os dados:
+function save() {
+    localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
